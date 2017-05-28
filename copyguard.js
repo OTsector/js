3a47f875650a5ef0b348dabb072818ed57dddcbe
OT <imot@protonmail.com>
2017-05-28 17:50:15 +0400

    How to:
    Insert with scripts tag and set it after <head> tag.

---
+++ copyguard.js
@@ -0,0 +1,16 @@
+//Will block: ctrl+s ctrl+u ctrl+c. you can finde more keycodes in google.
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+        if (e.ctrlKey && 
+            (e.keyCode === 67 || 
+             e.keyCode === 86 || 
+             e.keyCode === 85 || 
+             e.keyCode === 17 || 
+             e.keyCode === 79 || 
+             e.keyCode === 83 || 
+             e.keyCode === 117)) {
+            return false;
+        } else {
+            return true;
+        }
+};
